d06f52841116845f0e950251926307cd1330e61c
labstraction <labstraction@protonmail.com>
2025-02-26 12:22:31 +0100

    high order ex

---
+++ esercizi5.js
@@ -0,0 +1,24 @@
+const array1 = [23, 14, 56, 44, 3, 0, -1, 55, -12, 60, 8];
+
+//1) filtrare tutti i numeri minori o uguali a zero (FILTER)
+//2) convertire in gradi fahrenheit ipotizando che l'unita di partenza sia celsius (MAP)
+//3) fare la media dei valori (REDUCE)
+//4) restituire il valore più basso e il più alto (REDUCE)
+
+const array2 = ['pippo', 'pluto', 'paperino', 'qui', 'quo', 'qua', 'clarabella', 'minnie'];
+
+//5) filtrare tutti i nomi piu grandi di 4 caratteri (FILTER)
+//6) traformare tutte le stringe in maiuscolo (MAP)
+//7) mettere SOLO le iniziali maiuscole (MAP)
+//8) restiture una stringa composta dalle iniziale delle parole (REDUCE)
+//9) sommare tutti i caratteri di tutte le stringhe (REDUCE)
+//10) eliminare le vocali dalle stringhe (MAP)
+//11) restituire un array di numeri che sono le lunghezze delle singole parole (MAP)
+
+
+const array3 = [[1,2,3], [3,2,1,0], [0,0,0,0,0]];
+
+//12) restituire un array di numeri che sono le lunghezze dei sigoli array (MAP)
+//13) sommare tutti i numeri in tutti gli array (REDUCE)
+//14) sommare tutte le lunghezze degli array (REDUCE)
+//15) sommare tutti i numeri con indice uguale e restituire un array di risultati (REDUCE)
--- high-order-functions.js
+++ high-order-functions.js
@@ -113,55 +113,117 @@ const testArray2 = ['pippo', 'pluto', 'paperino', 'clarabella', 'minnie'];
 
 ///FILTER
 
-function keepEven(arrayOfNumbers){
-    const parkingArray = [];
+// function keepEven(arrayOfNumbers){
+//     const parkingArray = [];
+
+//     for (let i = 0; i < arrayOfNumbers.length; i++) {
+//         const number = arrayOfNumbers[i];
+
+//         if (number % 2 === 0) {
+//             parkingArray.push(number)
+//         }
+
+//     }
+
+//     return parkingArray;
+// }
+
+// const newArray11 = keepEven(testArray);
+// console.log('keep even', newArray11);
+
+
+// function filter(array, filteringFunction){
+    
+//     const parkingArray = [];
+
+//     for (let i = 0; i < array.length; i++) {
+//         const element = array[i];
+
+//         if (filteringFunction(element, i, array)) {
+//             parkingArray.push(element)
+//         }
+
+//     }
+
+//     return parkingArray;
+
+// }
+
+// function isEven(nbr){
+//     if(nbr % 2 === 0){
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+// // const newArray12 = filter(testArray, isEven);
+// const newArray12 = testArray.filter(isEven);
+// console.log('new keep even', newArray12);
+
+
+// //funzione che tiene dentro testArray tutti i numeri minori di 5
+// const newArray13 = testArray.filter(nbr => nbr < 5);
+// console.log('keep small numbers', newArray13);
+
+// //funzione che tiene dentro testArray2 tutte le parole più lunghe di 6 caratteri
+// const newArray14 = testArray2.filter(str => str.length > 6);
+// console.log('keep long string', newArray14);
+
+// //funzione che rimuove da testArray2 tutte le stringhe con indice pari
+// // const newArray15 = testArray2.filter((str, i) => i % 2 === 1)
+// const newArray15 = testArray2.filter((_, i) => i % 2 === 1)
+// console.log('keep odd index', newArray15);
+
+///REDUCE
+function sumAll(arrayOfNumbers){
+
+    let accoumulator = 0;
 
     for (let i = 0; i < arrayOfNumbers.length; i++) {
+
         const number = arrayOfNumbers[i];
 
-        if (number % 2 === 0) {
-            parkingArray.push(number)
-        }
+        accoumulator = accoumulator + number;
 
     }
 
-    return parkingArray;
+    return accoumulator;
 }
 
-const newArray11 = keepEven(testArray);
-console.log('keep even', newArray11);
+const sum = sumAll(testArray)
+console.log('sum all', sum);
 
 
-function filter(array, filteringFunction){
-    
-    const parkingArray = [];
+//const testArray = [2, 5, 7, 1, 0, 100, 103];
+
+
+function reduce(array, reducingFunction, startingAccumulator){
+                        //""
+    let accoumulator = startingAccumulator;
 
     for (let i = 0; i < array.length; i++) {
-        const element = array[i];
 
-        if (filteringFunction(element, i, array)) {
-            parkingArray.push(element)
-        }
-
+        const current = array[i];
+ 
+        accoumulator = reducingFunction(accoumulator, current, i, array);
     }
-
-    return parkingArray;
-
+    return accoumulator;
+}
+                        //14         //1
+function sum2Numbers(accoumulator, current){
+    const newAccumulator = accoumulator + current;
+    return newAccumulator;
 }
 
-function isEven(nbr){
-    if(nbr % 2 === 0){
-        return true;
-    } else {
-        return false;
-    }
-}
+// const sum1 = reduce(testArray, sum2Numbers, 0)
+const sum1 = testArray.reduce(sum2Numbers, 0)
+console.log('new sum all', sum1)
 
-// const newArray12 = filter(testArray, isEven);
-const newArray12 = testArray.filter(isEven);
-console.log('new keep even', newArray12);
+// concatena le stringhe di testArray2
+const sumString = testArray2.reduce((a, c) => a + c, "");
+console.log('sum all strings', sumString)
 
-
-//funzione che tiene dentro testArray tutti i numeri minori di 5
-const newArray13 = testArray.filter(nbr => nbr < 5)
-console.log('keep small', newArray13);
+// moltiplica tra loro tutti i numeri di testArray
+const product = testArray.reduce((a, c) => a * c, 1);
+console.log('multiplyAll', product);
