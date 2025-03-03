445725a1c3bba905afe972ddd615e5ac91f3a234
labstraction <labstraction@protonmail.com>
2025-03-03 12:48:27 +0100

    classes

---
+++ esercizi6.js
@@ -0,0 +1,8 @@
+//1) aggiungere al toString di tutte le classi gender e nationality
+//2) aggiungere a teacher le funzioni: bestStudent e StudentsMean
+//3) aggiungere la classe Principal che erediterà da human e come caratteristiche avrà:
+// - un array di teachers
+// - il nome della scuola
+//4) aggiungere al principal due funzioni: bestTeacher e bestStudent
+
+
