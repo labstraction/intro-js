ce2450aa8dd2474b5b00ce733bae0d594853922f
labstraction <labstraction@protonmail.com>
2025-03-03 13:25:13 +0100

    esercizi

--- esercizi6.js
+++ esercizi6.js
@@ -6,3 +6,30 @@
 //4) aggiungere al principal due funzioni: bestTeacher e bestStudent
 
 
+//---------------------------------------------------------
+
+
+
+//1)Crea una classe base ContoBancario:
+    //-La classe ContoBancario deve avere le seguenti proprietà: titolare (Human) saldo (numero)
+    //-La classe ContoBancario deve avere i seguenti metodi:
+        //-toString()
+        //-deposita(importo): Aggiunge l'importo al saldo.
+        //-preleva(importo): Sottrae l'importo dal saldo se ci sono fondi sufficienti, altrimenti stampa un messaggio di errore.
+
+//2)Crea una classe derivata ContoCorrente che eredita da ContoBancario:
+    //-La classe ContoCorrente deve avere le seguenti proprietà aggiuntive:
+        //-limiteSpesa (numero)
+    //-La classe ContoCorrente deve avere i seguenti metodi aggiuntivi:
+        //-paga(importo): Sottrae l'importo dal saldo se ci sono fondi sufficienti o se l'importo è entro il limite di spesa, altrimenti stampa un messaggio di errore.
+        // se il pagamento va a buon fine il metodo aggiunge al saldo il 2% dell'importo
+
+//3)Crea una classe derivata ContoRisparmio che eredita da ContoBancario:
+    //-La classe ContoRisparmio deve avere le seguenti proprietà aggiuntive:
+        //-sogliaBonus(numero)
+    //-La classe ContoRisparmio deve avere i seguenti metodi aggiuntivi:
+        //-applicaBonus(): Aggiunge un bonus del 2% (del saldo) al saldo se il saldo supera la soglia.
+
+
+
+
